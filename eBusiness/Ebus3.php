2fe95fd70554361eadbfbb8b0847ccf719065d5a
Ronan Keogh <117503266@umail.ucc.ie>
2018-02-23 12:17:28 +0000

    Finished CV page1

---
+++ eBusiness/Ebus3.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <Title> RECIEPT</Title>
+    </head>
+    <body>
+        <h4> RECIEPT</h4>
+        <?php
+        // Echo session variables that were set on previous page echo "Total is " . $ SESSION["total"] . ".";
+        ?>
+        </br>
+        <a HREF="../homepage.html"><input type="button" value="Home"></a>
+    </body>
+</html>
